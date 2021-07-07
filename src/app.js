8de39e9090deaf00d909fa7b971838f764855310
Shahriar Alif <shahriarcse26@gmail.com>
2021-07-08 00:39:00 +0600

    bug fixed

--- src/app.js
+++ src/app.js
@@ -24,14 +24,14 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Andrew Mead",
+    name: "Shahriar",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Andrew Mead",
+    name: "Shahriar",
   });
 });
 
@@ -39,7 +39,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
     title: "Help",
-    name: "Andrew Mead",
+    name: "Shahriar",
   });
 });
 
@@ -88,7 +88,7 @@ app.get("/products", (req, res) => {
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Shahriar",
     errorMessage: "Help article not found.",
   });
 });
@@ -96,7 +96,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Shahriar",
     errorMessage: "Page not found.",
   });
 });
